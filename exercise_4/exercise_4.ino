f131aecf6289432af212a2de00fde25b81deda45
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-08-28 20:01:00 +0100

    Add exercise 4
    Read temp/humidity values from a dht11 sensor and post them
    to a thingspeak channel

---
+++ exercise_4/exercise_4.ino
@@ -0,0 +1,64 @@
+#include <DHT.h>
+
+#include "wifi.h"
+#include "secrets.h"
+
+#define HTTP_HOST "api.thingspeak.com"
+#define HTTP_PORT 80
+
+DHT sensor(5, DHT11);
+float hum = 0, temp = 0;
+
+void setup() {
+  // initialize serial
+  Serial.begin(115200);
+
+  // initialize sensor
+  sensor.begin();
+
+  // connect to wifi
+  connectToWiFi(WLAN_SSID, WLAN_PASS);
+  printWifiStatus();
+}
+
+void loop() {
+  hum = sensor.readHumidity();
+  temp = sensor.readTemperature();
+
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.print("%; Temperature: ");
+  Serial.print(temp);
+  Serial.println("ºC");
+
+  WiFiClient client;
+  if (!client.connect(HTTP_HOST, HTTP_PORT)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  /* post thingspeak update payload to the server */
+  String message = String("") + F("field1=") + String(hum) + F("&field2=") + String(temp);
+  client.print("POST /update HTTP/1.1\r\n");
+  client.print("Host: api.thingspeak.com\r\n");
+  client.print("Connection: close\r\n");
+  client.print("X-ThingSpeakAPIKey: ");
+  client.print(THINGSPEAK_API_KEY);
+  client.print("\r\n");
+  client.print("Content-Type: application/x-www-form-urlencoded\r\n");
+  client.print("Content-Length: ");
+  client.print(message.length());
+  client.print("\r\n\r\n");
+  client.print(message);
+  client.print("\r\n");
+
+  /* read response from the server and print it to serial */
+  while (client.connected()) {
+    if (client.available()) {
+      Serial.print((char)client.read());
+    }
+  }
+  Serial.println("");
+
+  delay(20000);
+}
---
+++ exercise_4/wifi.cpp
@@ -0,0 +1,33 @@
+#include "wifi.h"
+
+void connectToWiFi(const char* ssid, const char* password) {
+  Serial.print("Connecting to wireless network ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\n\nConnected to ");
+  Serial.println(ssid);
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
