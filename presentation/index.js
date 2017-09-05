be4490d3b2ba5ffdba35e9c09d270033a46eda6b
Mário Carneiro <mario.carneiro@lifeonmars.pt>
2017-09-05 22:29:31 +0100

    refactor

--- presentation/index.js
+++ presentation/index.js
@@ -1,21 +1,23 @@
-function getResponseText(response) { return response.text(); }
+(function() {
 
-function getChapters() {
-  return Promise.props({
-    cover               : fetch("/chapters/cover.md").then(getResponseText),
-    introduction        : fetch("/chapters/introduction.md").then(getResponseText),
-    arduino_ecosystem   : fetch("/chapters/arduino_ecosystem.md").then(getResponseText),
-    setup               : fetch("/chapters/setup.md").then(getResponseText),
-    material            : fetch("/chapters/material.md").then(getResponseText),
-    exercises           : fetch("/chapters/exercises.md").then(getResponseText),
-  }).then(function(result) {
-    return [
-      result.cover,
-      result.introduction,
-      result.arduino_ecosystem,
-      result.setup,
-      result.material,
-      result.exercises,
-    ].join("\n");
+  const chapter = (n) => {
+    return fetch(`/chapters/${n}.md`)
+      .then(function(response) {
+        return response.text();
+      });
+  };
+
+  const source = () => {
+    const numbers = ["00", "01", "02", "03", "04", "05"];
+    return Promise.all(numbers.map(chapter))
+      .then((result) => result.join("\n"));
+  }
+
+  document.addEventListener("DOMContentLoaded", (event) => {
+    source()
+      .then((source) => {
+        remark.create({ source: source });
+      });
   });
-}
+
+})();
